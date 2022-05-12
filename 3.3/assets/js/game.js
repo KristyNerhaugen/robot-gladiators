b64d6c8ae5bd132a5d1a190933ab6410ca2e401d
kristynerhaugen <kristynerhaugen@gmail.com>
2022-05-12 16:03:34 -0500

    object optimization

--- 3.3/assets/js/game.js
+++ 3.3/assets/js/game.js
@@ -98,9 +98,7 @@ var fight = function(enemy) {
 // function to start a new game
 var startGame = function() {
   // reset player stats
-  playerInfo.health = 100;
-  playerInfo.attack = 10;
-  playerInfo.money = 10;
+  playerInfo.reset();
 
   // fight each enemy robot by looping over them and fighting them one at a time
   for (var i = 0; i < enemyInfo.length; i++) {
@@ -172,30 +170,34 @@ var shop = function() {
   switch (shopOptionPrompt) {
     case 'REFILL':
     case 'refill':
-      if (playerInfo.money >= 7) {
-        window.alert("Refilling player's health by 20 for 7 dollars.");
+     playerInfo.refillHealth();
+     break; 
+    //if (playerInfo.money >= 7) {
+     //   window.alert("Refilling player's health by 20 for 7 dollars.");
 
         // increase health and decrease money
-        playerInfo.health = playerInfo.health + 20;
-        playerInfo.money = playerInfo.money - 7;
-    }
-    else {
-        window.alert("You don't have enough money!");
-    }
-      break;
+     //   playerInfo.health = playerInfo.health + 20;
+     //   playerInfo.money = playerInfo.money - 7;
+   // }
+   // else {
+   //     window.alert("You don't have enough money!");
+   // }
+   //   break;
     case 'UPGRADE':
     case 'upgrade':
-      if (playerInfo.money >= 7) {
-        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+      playerInfo.upgradeAttack();
+      break;
+     // if (playerInfo.money >= 7) {
+      //  window.alert("Upgrading player's attack by 6 for 7 dollars.");
 
         // increase attack and decrease money
-        playerInfo.attack = playerInfo.attack + 6;
-        playerInfo.money = playerInfo.money - 7;
-    }
-    else {
-        window.alert("You don't have enough money!");
-    }
-      break;
+      //  playerInfo.attack = playerInfo.attack + 6;
+     //   playerInfo.money = playerInfo.money - 7;
+   // }
+    //else {
+       // window.alert("You don't have enough money!");
+    //}
+      //break;
     case 'LEAVE':
     case 'leave':
       window.alert('Leaving the store.');
@@ -222,7 +224,32 @@ var playerInfo = {
   name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
-  money: 10
+  money: 10, 
+  reset: function() {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  },
+  refillHealth: function() {
+    if (this.money >=7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+    this.health += 20;
+    this.money -= 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+  },
+  upgradeAttack: function() {
+    if(this.money >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+    this.attack += 6;
+    this.money -= 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+  }
 };
 
 var enemyInfo = [
